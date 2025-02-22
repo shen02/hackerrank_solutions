88453e6c4bbd7e6628ea95a1d36f3ac6e66be04b
Shen <71272044+shen02@users.noreply.github.com>
2025-02-22 01:13:24 +0000

    Removed stdin parsing components

---
+++ algorithms/Readme.md
@@ -0,0 +1,10 @@
+## URL to Challenges
+
+- Counting Valleys[]
+
+- Climbing the Leaderboard
+
+- Tower Breakers[https://www.hackerrank.com/challenges/one-week-preparation-kit-tower-breakers-1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-three]
+
+- Day Of Programmer
+
--- algorithms/day_of_programmer.cpp
+++ algorithms/day_of_programmer.cpp
@@ -2,9 +2,6 @@
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-
 /*
  * Complete the 'dayOfProgrammer' function below.
  *
@@ -22,44 +19,4 @@ string dayOfProgrammer(int year) {
         return to_string(12 + !((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))) + ".09." + to_string(year);
     }
     
-
-}
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string year_temp;
-    getline(cin, year_temp);
-
-    int year = stoi(ltrim(rtrim(year_temp)));
-
-    string result = dayOfProgrammer(year);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
